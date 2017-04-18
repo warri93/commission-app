148cc88108e0014c4c564418103da8f886f40b3e
melissawarrens <melissa.warrens@bewire.be>
2017-04-18 11:26:04 +0200

    first commit

---
+++ src/app/searchassignment/searchassignment.component.spec.ts
@@ -0,0 +1,12 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SearchassignmentComponent } from './searchassignment.component';
+import {CommissionBackEndService} from '../commission-back-end.service';
+
+describe('Component: Searchassignment', () => {
+  it('should create an instance', () => {
+    let component = new SearchassignmentComponent();
+    expect(component).toBeTruthy();
+  });
+});
